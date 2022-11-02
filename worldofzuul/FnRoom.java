df29ac8c1966e9038d12a4684305fcd914379648
CKSTool <caspa21@student.sdu.dk>
2022-11-02 13:13:28 +0100

    Creation of FnRoom class

---
+++ worldofzuul/FnRoom.java
@@ -0,0 +1,13 @@
+package worldofzuul;
+
+import java.util.HashMap;
+
+public class FnRoom extends Room{
+    public FnRoom(String description,HashMap<String, Room> exits){
+        super(description, exits);
+
+    }
+
+}
+
+
