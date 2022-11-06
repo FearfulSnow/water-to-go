81f4588cd0dad502e01651fc0ef765bd4633614d
DannerManner <dantheawesime24@gmail.com>
2022-11-06 20:41:17 +0100

    Added waterCost to Room class
    Added class WaterSource exstends Room

--- worldofzuul/WaterSource.java
+++ worldofzuul/WaterSource.java
@@ -1,5 +1,6 @@
 package worldofzuul;
 
+
 public class WaterSource extends Room {
     private int waterCost;
 
@@ -8,47 +9,22 @@ public class WaterSource extends Room {
         this.waterCost = waterCost;
     }
 
-    public int getWaterDiscount(Item pipes) {
-        Inventory waterSoucePipes = new Inventory();
-        return waterSoucePipes.getItem(pipes);
-        
+    public int getWaterDiscount() {
+        for (Item pipes: Inventory.items) {
+            if (pipes.equals("pipes")) {
+                int pipesAmount = pipes.getQuantity();
+                return pipesAmount;
+            }
+        }
     }
 
     //setWaterCost method
     public void setWaterCost(){
-        switch (getWaterDiscount()) {
-            case 1:
-                waterCost = 9;
-                break;
-            case 2:
-                waterCost = 8;
-                break;
-            case 3:
-                waterCost = 7;
-                break;
-            case 4:
-                waterCost = 6;
-                break;
-            case 5:
-                waterCost = 5;
-                break;
-            case 6:
-                waterCost = 4;
-                break;
-            case 7:
-                waterCost = 3;
-                break;
-            case 8:
-                waterCost = 2;
-                break;
-            case 9:
-                waterCost = 1;
-                break;
+        this.waterCost = 10 - getWaterDiscount();
         }
-    }
     //collectWater method
     public void collectWater(){ //Sets the water to full, which is 100
-        Inventory waterInv = new Inventory();
-        waterInv.setWater(100);
+        Inventory.setWater(Inventory.getMaxWater());
+
     }
 }
