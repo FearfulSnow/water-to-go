2adf8f3b5dc1346d3829ba67ac18b85ffbc0e7e7
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2022-11-29 15:46:10 +0100

    FnRoom ctrl & fxml, and add a game inst in main

--- worldofzuul/Controllers/FnRoomController.java
+++ worldofzuul/Controllers/FnRoomController.java
@@ -1,5 +1,44 @@
 package worldofzuul.Controllers;
 
-public class FnRoomController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import worldofzuul.FnRoom;
 
+import java.io.IOException;
+
+public class FnRoomController {
+    SceneController sceneController = new SceneController();
+    double currentProgress = 0.0;
+    @FXML
+    private TextField textEX;
+    @FXML
+    private ProgressBar TaskProgressBar;
+
+    @FXML
+    void goHome(ActionEvent event) throws IOException {
+        sceneController.goHome(event);
+    }
+
+    @FXML
+    void completeTask(ActionEvent event) throws IOException {
+        if (FnRoom.currentTask == null){
+            textEX.setText("You have no tasks to complete");
+        } else if (FnRoom.currentTask.completeTask()) {
+            FnRoom.currentTask = null;
+            textEX.setText("Well done! You have completed the task. Feel free to accept the next one!");
+            currentProgress += 1/ (double) FnRoom.getTaskArrayList().size();
+            TaskProgressBar.setProgress(currentProgress);
+            //if (//win)
+        } else {
+            String str = "\nNeeds " + FnRoom.currentTask.getRequirement().getQuantity() + " " + FnRoom.currentTask.getRequirement().getName() + "\n";
+            textEX.setText(str);
+        }
+    }
+    @FXML
+    void acceptTask(ActionEvent event) throws IOException {
+        FnRoom.giveTask();
+        textEX.setText(FnRoom.currentTask.getDescription());
+    }
 }
