653b64f2e248ef6555c7451d336c8d3bc54f7ba0
joachimhviid <joac1099@gmail.com>
2022-12-03 11:08:51 +0100

    Cleanup

--- worldofzuul/Controllers/FnRoomController.java
+++ worldofzuul/Controllers/FnRoomController.java
@@ -2,9 +2,7 @@ package worldofzuul.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import worldofzuul.FnRoom;
 
@@ -22,15 +20,6 @@ public class FnRoomController {
     @FXML
     void completeTask(ActionEvent event) throws IOException {
         FnRoom.completeCurrentTask();
-        //if (FnRoom.currentTask == null){
-        //    textEX.setText("You have no tasks to complete");
-        //} else if (FnRoom.currentTask.completeTask()) {
-        //    FnRoom.currentTask = null;
-        //    textEX.setText("Well done! You have completed the task. Feel free to accept the next one!");
-        //} else {
-        //    String str = "\nNeeds " + FnRoom.currentTask.getRequirement().getQuantity() + "x " + FnRoom.currentTask.getRequirement().getName() + "\n";
-        //    textEX.setText(str);
-        //}
         update();
     }
     @FXML
